e799c800b3221761fe17522541fcb7ccf91a99cc
Michael-LAI JINFENG <cihv2@163.com>
2018-07-28 10:51:03 +0800

    EGL demo

---
+++ ValifyRustDemo/SolarSystem/ViewController.m
@@ -0,0 +1,66 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+@property(strong, nonatomic) EAGLContext *context;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    [self _setupEGLContext];
+}
+
+- (void)_setupEGLContext {
+    self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+    if (!self.context) {
+        NSLog(@"Failed to create ES context, no need to continue");
+        exit(0);
+    }
+    
+    GLKView *view = (GLKView *) self.view;
+    view.context = self.context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [EAGLContext setCurrentContext:self.context];
+}
+
+- (void)dealloc {
+    [self _tearDownGL];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+    if ([self isViewLoaded] && ([[self view] window] == nil)) {
+        [self _tearDownGL];
+        
+        self.context = nil;
+        self.view = nil;
+    }
+
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (void)_tearDownGL {
+    [EAGLContext setCurrentContext:self.context];
+    if ([EAGLContext currentContext] == self.context) {
+        [EAGLContext setCurrentContext:nil];
+    }
+    self.context = nil;
+}
+
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    [EAGLContext setCurrentContext:self.context];
+    [view bindDrawable];
+    
+    // ----------- issue your draw call
+    
+}
+
+@end
